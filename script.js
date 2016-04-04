02eb828c91fcc34ae6d3a2f0fb44d5a928861fb8
Flixern <felix.bergh@gmail.com>
2016-04-04 13:33:47 +0200

    Dark knight manus
    Only temporary

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 (function () {
     'use strict';
     
-    console.log("HEI HEI")
+    var el = function (id) {
+        document.getElementById(id);
+    };
     
+    // modell
+    var spiller = "X";
+    var trekk = new Array(9);
     
+    const brett = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+    
+    var oppdater = function(knappTrykket) {
+        console.log("Knapp trykket med id: "  + knappTrykket);
+        settspiller();
+        lagreTrekket
+        vis();
+    };
+    
+    var settSpiller = function () {
+        return (spiller === "0") ? spiller = "X" : spiller = "0";
+    };
+    
+    var lagreTrekk = function () {
+        return trekk[knappTrykket] = spiller;
+    };
+    
+    var vis = function() {
+        tegnMerkerPaaBrettet();
+    };
+    
+    var tegnMerkerPaaBrettet = function() {
+        brett.forEach(tegnMerke);
+    };
+    
+    var tegnMerke = function (felt) {
+        el(felt).innerHTML = trekk[felt];
+    };
+   
+     el("0").addEventListener("click", function () {
+    oppdater
+});
     
 })();
